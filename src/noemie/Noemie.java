6e93149e4d0db9976c8d2ea336e7c2a5fc09ea4f
Noëmie <Nomie@172.16.124.99>
2014-12-04 09:58:03 +0100

    Premier commit de base sur la classe Lapin du projet Noemie

---
+++ src/noemie/Noemie.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package noemie;
+
+/**
+ *
+ * @author Noëmie
+ */
+public class Noemie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello word !");
+        Lapin lap = new Lapin("Panpan", 4);
+        lap.crier();
+        
+    }
+    
+}
